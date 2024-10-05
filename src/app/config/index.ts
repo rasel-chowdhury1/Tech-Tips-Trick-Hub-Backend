20526c494a570b85f1ec4b3f4a3541855f1aa0cb
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-10-05 08:18:48 +0800

    config and env file setup done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,4 +5,11 @@ export default {
   db_url: process.env.DB_URL,
   node_dev: process.env.NODE_ENV,
   bcrypt_salt_number: process.env.BCRYPT_SALT_NUMBER,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  reset_pass_ui_link: process.env.RESET_PASS_UI_LINK,
+  client_url: process.env.CLIENT_URL,
 }
+
